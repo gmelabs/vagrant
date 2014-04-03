f8ef69ca48c0100fed7f8f25f4fe5700bf885522
miguel.infante.munoz <miguel.infante.munoz@accenture.com>
2014-04-03 18:27:40 +0200

    Modificados vagrantup.x para recoger IP de Nexus Server de fichero. Creado nexus{BUILD}.properties con la URL del servidor Nexus tal y como deberá verse desde los entornos levantados. Para que funcione el puppet se tendrá que hacer una copia de éste llamándolo nexus.properties.
    Cuando se ejecute mediante el script vagrantup.sh esta copia se realiza
    de forma automática:
    ---------------------------------------------------------------------
    $ export VM_MEMORY=1024
    $ cp puppet/modules/base/files/nexus{BUILD}.properties
    >    puppet/modules/base/files/nexus.properties
    $ vagrant up
    ---------------------------------------------------------------------
    
    Cuando se ejecute en local, con vagrantup.bat se considerará que existe
    un repositorio Nexus en Local, escuchando en 0.0.0.0:8081, por lo que se
    obtendrá una IP de la máquina local y se generará el fichero
    nexus{LOCAL}.properties para, a continuación, de forma análoga al script
    Linux, copiarse sobre nexus.properties
    ---------------------------------------------------------------------
    > SET VM_MEMORY=400
    > [ obtencion IP > nexus{LOCAL}.properties ]
    > copy /Y ^
    ?     "puppet\modules\base\files\nexus{LOCAL}.properties" ^
    ?     "puppet\modules\base\files\nexus.properties"
    > vagrant up
    ---------------------------------------------------------------------
    Cuando no se disponga de un Nexus en local, la opción más práctica será
    tunelear el Nexus real desde cygwin/mobaXterm/...
    ssh -N {usuario}@10.192.104.12 -L 0.0.0.0:8081:127.0.0.1:8081

--- vagrantup.bat
+++ vagrantup.bat
@@ -1,3 +1,53 @@
-SET VM_MEMORY=400
+@echo off
 
+SET VM_MEMORY=400
+echo INFO - Setting VM_MEMORY=%VM_MEMORY%
+
+rem -----------------------------------------------------------------------------------------------
+rem Obtaining local IP. Nexus Server should be local. If not, a SSH tunnel may be required
+rem -----------------------------------------------------------------------------------------------
+echo WARN - Local execution! Getting local IP for Nexus Server. WARN - SSH TUNNEL MAY BE REQUIRED!
+set str=
+ipconfig | findstr /C:"   IPv4" > .tmp
+for /f "tokens=*" %%i in ('type .tmp') do (
+	call :getip "%%i"
+	goto :break-1
+)
+:break-1
+rem -----------------------------------------------------------------------------------------------
+
+echo INFO - Copying nexus{LOCAL}.properties To nexus.properties
+copy /Y "puppet\modules\base\files\nexus{LOCAL}.properties" puppet\modules\base\files\nexus.properties
+
+echo -------------------------------------------------------------------------
+echo INFO - vagrant up
+echo -------------------------------------------------------------------------
+@echo on
 vagrant up
+@echo off
+echo.
+echo -------------------------------------------------------------------------
+goto :end
+
+
+rem ###############################################################################################
+rem GetIP: Creates nexus{LOCAL}.properties file with the IP contained in ipconfig output file line
+rem ###############################################################################################
+:getip
+set str=%1
+set str=%str:~1,-1%
+set str=%str: =%
+for /f "tokens=2 delims=: " %%a in ("%str%") do (
+	echo WARN - Pushing NEXUS_URL=%%a:8081 to 'nexus{LOCAL}.properties'.
+	echo WARN -         ========= ^^
+	echo WARN - A SSH Tunnel may be required. Example:
+	echo WARN -  ssh -N %USERNAME%@10.192.104.12 -L 0.0.0.0:8081:127.0.0.1:8081
+	echo NEXUS_URL=%%a:8081> "puppet\modules\base\files\nexus{LOCAL}.properties"
+)
+del .tmp
+goto :EOF
+rem ###############################################################################################
+
+:end
+echo INFO - Finished!
+exit 0
--- vagrantup.sh
+++ vagrantup.sh
@@ -2,4 +2,6 @@
 
 export VM_MEMORY=1024
 
+cp puppet/modules/base/files/nexus{BUILD}.properties puppet/modules/base/files/nexus.properties
+
 vagrant up
